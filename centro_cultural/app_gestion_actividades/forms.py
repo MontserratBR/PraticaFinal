997c97e7f898916b38fa8d015cbedb8f33108264
Montserrat <lesliemontserratbarrera@gmail.com>
2026-05-14 00:20:09 +0200

    Se agregarón los formulario

---
+++ centro_cultural/app_gestion_actividades/forms.py
@@ -0,0 +1,49 @@
+from django import forms
+from .models import Usuario,Monitor,Sala,Actividad,Inscripcion,ResponsableSala
+
+
+class UsuarioForm(forms.ModelForm):
+
+    class Meta:
+        model = Usuario
+        fields = '__all__'
+
+class MonitorForm(forms.ModelForm):
+
+    class Meta:
+        model = Monitor
+        fields = '__all__'
+
+class ResponsableSalaForm(forms.ModelForm):
+
+    class Meta:
+        model = ResponsableSala
+        fields = '__all__'
+
+class SalaForm(forms.ModelForm):
+
+    class Meta:
+        model = Sala
+        fields = '__all__'
+
+class ActividadForm(forms.ModelForm):
+    class Meta:
+        model = Actividad
+        fields = '__all__'
+        widgets = {
+            'horario': forms.DateTimeInput(
+                attrs={'type': 'datetime-local'}
+            ),
+
+            'descripcion': forms.Textarea(
+                attrs={'rows': 4}
+            ),
+
+            'salas_secundarias': forms.CheckboxSelectMultiple()
+        }
+
+class InscripcionForm(forms.ModelForm):
+
+    class Meta:
+        model = Inscripcion
+        fields = '__all__'
